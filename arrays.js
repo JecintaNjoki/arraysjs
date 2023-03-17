8ea8790e4a51ec866a7a2fb913bb08b14326922c
JecintaNjoki <jecintanjokikimanii@gmail.com>
2023-03-17 21:14:30 +0300

    firstcommit

---
+++ arrays.js
@@ -0,0 +1,29 @@
+
+//Find the last element of the following arrays.
+//arr1 = [3,7,34,90,12]
+const arr1=[3,7,34,90,12];
+const lastItem=arr1.slice(-1);
+console.log(lastItem);
+
+//arr2 = [true, "green", "where",12,56]
+const arr2=[true,"green","where",12,56];
+const lastelement=arr2.slice(-1);
+console.log(lastelement);
+//Write a JS program that will join the following array elements into a string
+//myPets = ["Cow", "Python", "Snake", "Dog"];
+const myPets=["cow","python","snake","dog"];
+console.log(myPets.join());
+
+//Write a JS script to sort the following array items
+//var arr3 = [ -5, 9, 5, 3, 2, -3, 6, 8, 4, 1 ];
+const arr3=[-5,9,5,3,2,-3,6,8,4,1];
+arr3.sort();
+console.log(arr3);
+
+
+
+
+ let word="lufituable";
+ let x= word.split("");
+ x.sort();
+ console.log(x);
